754f3651b8a0e1af579eb4015272f0cdd059c619
JustinMartinDev <you@example.com>
2019-02-04 20:03:25 +0100

    Updates

---
+++ precache-manifest.207945bfc65f64a5c319ff9d399e7716.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed5e9bdec79e53000511cde92851c40a",
+    "url": "/smart-borne-web/static/media/nasa-logo.ed5e9bde.png"
+  },
+  {
+    "revision": "c7dc52c073937b2916b4a6632d3548d2",
+    "url": "/smart-borne-web/static/media/grey.c7dc52c0.jpg"
+  },
+  {
+    "revision": "83c103ca227ae9494ffd",
+    "url": "/smart-borne-web/static/js/runtime~main.83c103ca.js"
+  },
+  {
+    "revision": "aca4f4d8729f3579e0d3",
+    "url": "/smart-borne-web/static/js/main.aca4f4d8.chunk.js"
+  },
+  {
+    "revision": "fcea78fc72ace6696fa6",
+    "url": "/smart-borne-web/static/js/1.fcea78fc.chunk.js"
+  },
+  {
+    "revision": "aca4f4d8729f3579e0d3",
+    "url": "/smart-borne-web/static/css/main.98087ed1.chunk.css"
+  },
+  {
+    "revision": "9721e6f8907d07daa116e25a3ef16e7e",
+    "url": "/smart-borne-web/index.html"
+  }
+];
